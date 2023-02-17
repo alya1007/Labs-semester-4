35a095e57c3ce5ac32fbb1969531f87bc143f407
alya1007 <alexandra.konjevic@isa.utm.md>
2023-02-17 13:58:04 +0200

    lfaf lab

--- LFAF/src/FiniteAutomaton.cs
+++ LFAF/src/FiniteAutomaton.cs
@@ -2,8 +2,41 @@ namespace src
 {
     class FiniteAutomaton
     {
-        public FiniteAutomaton()
+        public List<string> States { get; set; }
+        public List<char> Alphabet { get; set; }
+        public Dictionary<(string, char), string> Transitions { get; set; }
+        public string StartState { get; set; }
+        public List<string> FinalStates { get; set; }
+
+        public FiniteAutomaton(List<string> states, List<char> alphabet, Dictionary<(string, char), string> transitions, string startState, List<string> finalStates)
         {
+            States = states;
+            Alphabet = alphabet;
+            Transitions = transitions;
+            StartState = startState;
+            FinalStates = finalStates;
+        }
+
+        public bool CanGenerateString(string input)
+        {
+            var currentState = StartState;
+
+            foreach (var symbol in input)
+            {
+                if (!Alphabet.Contains(symbol))
+                {
+                    return false; // Reject input if it contains a symbol not in the alphabet
+                }
+
+                if (!Transitions.ContainsKey((currentState, symbol)))
+                {
+                    return false; // Reject input if there is no transition from the current state for the current symbol
+                }
+
+                currentState = Transitions[(currentState, symbol)];
+            }
+
+            return FinalStates.Contains(currentState); // Accept input if the final state is in the list of final states
         }
     }
 }
