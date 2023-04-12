f7ff86f5574aa1babaa9f27745cc325a30504615
alya1007 <alexandra.konjevic@isa.utm.md>
2023-04-12 16:19:16 +0300

    lfaf epsilon removal

--- LFAF/src/Program.cs
+++ LFAF/src/Program.cs
@@ -5,12 +5,36 @@ namespace src
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            string input = "docker build -t myimage:latest .";
-            Lexer lexer = new Lexer(input);
-            lexer.PrintTokens();
+            // string input = "docker build -t myimage:latest .";
+            // Lexer lexer = new Lexer(input);
+            // lexer.PrintTokens();
+
+            // initialize the grammar
+            var nonTerminalSymbols = new List<string> { "S", "A", "B", "C" };
+            var terminalSymbols = new List<string> { "a", "b", "c" };
+            var startingSymbol = "S";
+            var rules = new List<Production> {
+                new Production(new string[] { "S" }, new string[] { "A", "B", "C", "D", "A" }),
+                new Production(new string[] { "A" }, new string[] { "" }),
+                new Production(new string[] { "B" }, new string[] { "b" }),
+                new Production(new string[] { "C" }, new string[] { "c" }),
+                new Production(new string[] { "C" }, new string[] { "" }),
+                new Production(new string[] { "B" }, new string[] { "" }),
+            };
+            var normalizedGrammar = new NormalizedGrammar(nonTerminalSymbols, terminalSymbols, startingSymbol, rules);
+            var list = normalizedGrammar.GenerateStringArrays(normalizedGrammar.Rules[1].LeftSide[0], normalizedGrammar.Rules[0].RightSide);
+            var listWithoutDuplicates = normalizedGrammar.RemoveDuplicateArrays(list);
+            // print all elements of list
+            foreach (var item in list)
+            {
+                foreach (var element in item)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- LFAF/src/grammar/NormalizedGrammar.cs
+++ LFAF/src/grammar/NormalizedGrammar.cs
@@ -5,22 +5,83 @@ namespace src
         public NormalizedGrammar(List<string> nonTerminalSymbols, List<string> terminalSymbols, string startingSymbol, List<Production> rules) : base(nonTerminalSymbols, terminalSymbols, startingSymbol, rules)
         {
         }
-
-        public void EliminateEpsilonProductions()
+        public void RemoveEpsilonProductions()
         {
-            var toRemove = new List<Production>();
-            foreach (var prod in Rules)
+            List<Production> epsilonProductions = EpsilonProductionList();
+            List<Production> rulesContainingEpsilon = new List<Production>();
+            foreach (Production production in epsilonProductions)
             {
-                if (prod.RightSide.Length == 1 && prod.RightSide[0] == "")
-                {
-                    toRemove.Add(prod);
-                }
+                Rules.Remove(production);
             }
-            foreach (var prod in toRemove)
+            foreach (var rule in Rules)
             {
-                Rules.Remove(prod);
+            }
+            foreach (var production in epsilonProductions)
+            {
+                var listToAdd = GenerateStringArrays(production.LeftSide[0], production.RightSide);
             }
         }
+        public List<Production> EpsilonProductionList()
+        {
+            List<Production> epsilonProductions = new List<Production>();
+            foreach (Production production in Rules)
+            {
+                if (production.LeftSide.Length == 1 && production.RightSide.Length == 1 && production.RightSide[0] == "")
+                {
+                    epsilonProductions.Add(production);
+                }
+            }
+            return epsilonProductions;
+        }
 
+        public List<string[]> GenerateStringArrays(string charToReplace, string[] inputArray)
+        {
+            List<string[]> outputList = new List<string[]>();
+            // Loop through each element of the input array
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                // If the element contains the char to replace
+                if (inputArray[i].Contains(charToReplace[0]))
+                {
+                    // Create a new string array with the same length as the input array
+                    string[] newStringArray = new string[inputArray.Length - 1];
+                    // Copy all the elements of the input array to the new array, except the element with the char to replace
+                    for (int j = 0, k = 0; j < inputArray.Length; j++)
+                    {
+                        if (j != i)
+                        {
+                            newStringArray[k] = inputArray[j];
+                            k++;
+                        }
+                    }
+                    if (!outputList.Contains(newStringArray))
+                    {
+                        // Add the new string array to the output list
+                        outputList.Add(newStringArray);
+                    }
+                    // Recursively call the same method with the new string array as input
+                    outputList.AddRange(GenerateStringArrays(charToReplace[0].ToString(), newStringArray));
+                }
+            }
+            return RemoveDuplicateArrays(outputList);
+        }
+
+        public List<string[]> RemoveDuplicateArrays(List<string[]> inputList)
+        {
+            HashSet<string> set = new HashSet<string>();
+            List<string[]> outputList = new List<string[]>();
+            foreach (string[] arr in inputList)
+            {
+                // Check if the current array's contents are already in the set
+                string arrString = string.Join(",", arr);
+                if (!set.Contains(arrString))
+                {
+                    // If not, add the array to the output list and the set
+                    outputList.Add(arr);
+                    set.Add(arrString);
+                }
+            }
+            return outputList;
+        }
     }
 }
