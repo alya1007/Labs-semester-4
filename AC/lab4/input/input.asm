5ed89f9286988776333dbccec0d2e96f8a0381b1
alya1007 <alexandra.konjevic@isa.utm.md>
2023-04-25 02:14:06 +0300

    concat update lab4

--- AC/lab4/input/input.asm
+++ AC/lab4/input/input.asm
@@ -1,5 +1,5 @@
-section .data ; Start of the .data section
-    prompt db 'Enter a string: ' ; Define a prompt message as a null-terminated string of ASCII characters
+section .data
+    prompt db 'Enter a string: ' ; Define a prompt message
     prompt_len equ $-prompt ; Compute the length of the prompt message
     buffer times 256 db 0 ; Define a buffer to store the user's input, with a maximum size of 256 bytes
 
@@ -12,22 +12,22 @@ _start: ; Start of the _start subroutine
     mov ebx, 1 ; Move the file descriptor for standard output to EBX
     mov ecx, prompt ; Move the address of the prompt message to ECX
     mov edx, prompt_len ; Move the length of the prompt message to EDX
-    int 0x80 ; Call the Linux kernel's system call handler
+    int 0x80
 
     ; Read input from the user
     mov eax, 3 ; Move the system call number for read to EAX
     mov ebx, 0 ; Move the file descriptor for standard input to EBX
     mov ecx, buffer ; Move the address of the buffer to store the user's input to ECX
     mov edx, 255 ; Move the maximum number of bytes to read to EDX
-    int 0x80 ; Call the Linux kernel's system call handler
+    int 0x80
 
     ; Display the user's input
     mov eax, 4 ; Move the system call number for write to EAX
     mov ebx, 1 ; Move the file descriptor for standard output to EBX
     mov ecx, buffer ; Move the address of the buffer containing the user's input to ECX
-    int 0x80 ; Call the Linux kernel's system call handler
+    int 0x80
 
     ; Exit the program
     mov eax, 1 ; Move the system call number for exit to EAX
     xor ebx, ebx ; Clear EBX (equivalent to setting it to zero)
-    int 0x80 ; Call the Linux kernel's system call handler to terminate the program
+    int 0x80
